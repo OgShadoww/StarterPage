4a9fdccd300c3a37bed5c3db5d4269370e852790
OgShadoww <galenzao@gmail.com>
2025-07-18 23:41:35 +0300

    Mini adaptive

--- js/commands/set.js
+++ js/commands/set.js
@@ -1,3 +1,7 @@
-export default function set(args, terminal) {
+import { printOutput } from "../utils/terminal.js";
 
+export default function set(args, terminal) {
+  printOutput("This is setting function", terminal);
+  printOutput("You can set any configuration of this page", terminal);
+  printOutput("Configs:", terminal);
 }
